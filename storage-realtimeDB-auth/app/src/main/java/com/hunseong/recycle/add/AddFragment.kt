9991f14e2d5e2dca5aec6a6604afc2edabf524d4
훈성 <phunseong@gmail.com>
2022-01-04 19:52:22 +0900

    feat: Firebase REaltimeDB add 구현

---
+++ storage-realtimeDB-auth/app/src/main/java/com/hunseong/recycle/add/AddFragment.kt
@@ -0,0 +1,157 @@
+package com.hunseong.recycle.add
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import com.hunseong.recycle.databinding.FragmentAddBinding
+import com.hunseong.recycle.extension.DBKey.DB_ARTICLES
+import com.hunseong.recycle.home.Article
+
+class AddFragment : Fragment() {
+    private lateinit var binding: FragmentAddBinding
+
+    private var selectedUri: Uri? = null
+
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
+
+    private val storage: FirebaseStorage by lazy {
+        Firebase.storage
+    }
+
+    private val articleDB: DatabaseReference by lazy {
+        Firebase.database.reference.child(DB_ARTICLES)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentAddBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        imageAddBtn.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // 권한 승인, 갤러리 접근
+                    startContentProvider()
+                }
+
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    // 교육용 팝업
+                    showPermissionPopup()
+                }
+
+                else -> {
+                    // 최초 권한 요청
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        1000
+                    )
+                }
+            }
+        }
+
+        addBtn.setOnClickListener {
+            val title = titleEt.text.toString()
+            val price = priceEt.text.toString() + "원"
+            val sellerId = auth.currentUser?.uid.orEmpty()
+
+            val article = Article(sellerId, title, System.currentTimeMillis(), price, "")
+            articleDB.push().setValue(article)
+
+            findNavController().navigateUp()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        if (requestCode == 1000) {
+            // 권한 승인
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startContentProvider()
+            } else {
+                Toast.makeText(requireContext(), "권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startContentProvider() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+
+        startActivityForResult(intent, 2000)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) return
+
+        if (requestCode == 2000) {
+            val uri = data?.data
+            if (uri != null) {
+                binding.photoIv.setImageURI(uri)
+                selectedUri = uri
+            } else {
+                Toast.makeText(requireContext(), "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(requireContext(), "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showPermissionPopup() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("권한이 필요합니다.")
+            .setMessage("사진을 가져오기 위해 권한에 동의해주세요.")
+            .setPositiveButton("동의") { _, _ ->
+                requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1000
+                )
+            }
+            .setNegativeButton("거부") { _, _ ->
+                Toast.makeText(requireContext(), "권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+            .setCancelable(false)
+            .create()
+            .show()
+
+    }
+}
--- storage-realtimeDB-auth/app/src/main/java/com/hunseong/recycle/home/HomeFragment.kt
+++ storage-realtimeDB-auth/app/src/main/java/com/hunseong/recycle/home/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.hunseong.recycle.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ChildEventListener
@@ -23,12 +26,18 @@ class HomeFragment : Fragment() {
     private val articleList = mutableListOf<Article>()
     private val listener = object : ChildEventListener {
         override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            Log.e("lilili", "childAdd: ", )
+
             val article = snapshot.getValue(Article::class.java) ?: return
             articleList.add(article)
             articleAdapter.submitList(articleList)
+            articleAdapter.notifyItemChanged(articleList.lastIndex)
         }
 
-        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+            Log.e("lilili", "childChange: ", )
+
+        }
 
         override fun onChildRemoved(snapshot: DataSnapshot) {}
 
@@ -58,15 +67,33 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        articleDB = Firebase.database.reference.child(DB_ARTICLES)
+        initViews()
 
+    }
+
+    private fun initViews() {
+
+        binding.fab.setOnClickListener {
+
+            // todo 로그인 기능 구현 후 주석 해제
+//            if (auth.currentUser != null) {
+                val directions = HomeFragmentDirections.homeToAdd()
+                findNavController().navigate(directions)
+//            } else {
+//                Snackbar.make(it, "로그인 후 사용해주세요.", Snackbar.LENGTH_SHORT).show()
+//            }
+        }
+
+        articleDB = Firebase.database.reference.child(DB_ARTICLES)
         articleDB.addChildEventListener(listener)
+
         binding.recyclerView.adapter = articleAdapter
     }
 
     override fun onResume() {
+        Log.e("lilili", "onResume: ", )
         super.onResume()
-        articleAdapter.notifyDataSetChanged()
+        articleList.clear()
     }
 
     override fun onDestroyView() {
